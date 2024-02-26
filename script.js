9fa9ad42e711539a17c85d9c26303677b87c8b91
Andrew-K-M <andrew@mankins.org>
2024-02-26 10:15:22 -0800

    use and change color of pixels

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-const container = document.querySelector(".grid");
+const container = document.querySelector(".grid")
+
 let size= 16*16;
+/*
 function makeGrid(){
     for(let i =0;i<size;i++){
         let pixel = document.createElement('div');
         pixel.className="pixel";
-        pixel.style.backgroundColor="";
+        pixel.style.backgroundColor="blue";
         pixel.textContent=i;
         container.appendChild(pixel)
     }
 }
+
+const p = document.querySelectorAll('.pixel');
+container.addEventListener('mouseover',()=>{
+    container.style.backgroundColor="gray";
+});
+
 makeGrid();
+*/
+for(let i =0;i<size;i++){
+    let pix = document.createElement('div');
+    pix.className="pixel";
+    container.appendChild(pix);
+
+    pix.addEventListener('mouseover',()=>{
+    pix.style.backgroundColor=document.getElementById('colorSelect').value;
+    });
+}
+
