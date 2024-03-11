cbfd7c32b72dbb2f10027e6cfb514fc9f660dc4d
Andrew-K-M <andrew@mankins.org>
2024-03-11 10:27:31 -0700

    added slider to chnage the rez of grid

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
 const container = document.querySelector(".grid")
+sizeSelect = document.getElementById('myRange');
+sizeInfo = document.getElementById('sizeInfo');
+let numSize= sizeSelect.value; //16 default
+//let numSize = 64;
+let size= numSize*numSize;
+const boxSize = 500;
 
-let size= 16*16;
-/*
 function makeGrid(){
+    let pixelSize=(boxSize/numSize).toString()+'px';
+    container.style.gridTemplateColumns = `repeat(${numSize}, ${pixelSize})`;
+    container.style.gridTemplateRows = `repeat(${numSize}, ${pixelSize})`;
     for(let i =0;i<size;i++){
-        let pixel = document.createElement('div');
-        pixel.className="pixel";
-        pixel.style.backgroundColor="blue";
-        pixel.textContent=i;
-        container.appendChild(pixel)
+        let pix = document.createElement('div');
+        pix.className="pixel";
+        container.appendChild(pix);
+
+        pix.addEventListener('mouseover',()=>{
+        pix.style.backgroundColor=document.getElementById('colorSelect').value;
+        });
     }
 }
 
-const p = document.querySelectorAll('.pixel');
-container.addEventListener('mouseover',()=>{
-    container.style.backgroundColor="gray";
-});
+function updateGrid() {
+    numSize = sizeSelect.value;
+    size = numSize ** 2;
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    makeGrid();
+}
 
-makeGrid();
-*/
-
-
-for(let i =0;i<size;i++){
-    let pix = document.createElement('div');
-    pix.className="pixel";
-    container.appendChild(pix);
-
-    pix.addEventListener('mouseover',()=>{
-    pix.style.backgroundColor=document.getElementById('colorSelect').value;
-    });
+sizeSelect.onchange = function() {
+    sizeInfo.textContent = `Value: ${numSize}x${numSize}`
+    updateGrid();
 }
 
 function onClick(){
     let pixList= container.querySelectorAll(".pixel");
     for(let i =0;i<size;i++){
-        pixList[i].style.backgroundColor="gray";
+        pixList[i].style.backgroundColor="lightgray";
     }
 }
+
+makeGrid();
